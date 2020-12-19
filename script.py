227d7cfd91a6905b2b259c6de2ab363c4f3895b4
JDBentley <SneakyEskimo@outlook.com>
2020-12-18 21:35:34 -0500

    calculations of squares

--- script.py
+++ script.py
@@ -41,11 +41,27 @@ runtime = 120
 rating_out_of_10 = 7.5
 
 
+
 print(25 * 68 + 13 / 28)
 
 
 
-uilt_width = 8
+quilt_width = 8
 quilt_length = 12
 quilt_length = 8
 print(quilt_width * quilt_length)
+
+
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+
+# 7x7 quilt
+print(7 ** 2)
+
+# 8x8 quilt
+print(8 ** 2)
+
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6 ** 2 * 6)
