4fb07f00ecb6684fb5fb57c15e6b21564e01bdde
JDBentley <SneakyEskimo@outlook.com>
2020-12-18 21:26:44 -0500

    Hello world + variables

---
+++ script.py
@@ -0,0 +1,24 @@
+#Generic hello world!
+my_name = 'Jesse'
+print('Hello and welcome ' + my_name + '!')
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = 'lunch'
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = 'dinner'
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
