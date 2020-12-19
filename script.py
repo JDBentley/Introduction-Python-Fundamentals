c16822088e0ba30da5647cdd673b934351c21a08
JDBentley <SneakyEskimo@outlook.com>
2020-12-18 21:29:41 -0500

    Integer and Float

--- script.py
+++ script.py
@@ -27,3 +27,10 @@ print(meal)
 print('This message has mismatched quote marks!')
 print('Abracadabra')
 
+# Define the release and runtime integer variables below:
+release_year = 2020
+runtime = 120
+
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 7.5
